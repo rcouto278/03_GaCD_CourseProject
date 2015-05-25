00065ee81de0077c387d043034d10a56e997b746
Rodrigo Couto <rcouto278@gmail.com>
2015-05-25 06:12:15 -0300

    WIP

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,14 @@
-if(!file.exists("./data")){dir.create("./data")}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/uci_har_data.zip",method="curl")
-unzip("./data/uci_har_data.zip", exdir="./data")
+if(!file.exists("./data")){
+  dir.create("./data")
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl,destfile="./data/uci_har_data.zip",method="curl")
+  unzip("./data/uci_har_data.zip", exdir="./data")
+}
 
-# restData <- read.csv("./data/restaurants.csv")
+testData <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+trainData <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+allData <- rbind(testData, trainData)
+
+
+
+write.table( allData, "./data/merged_data.txt", row.name=FALSE)
